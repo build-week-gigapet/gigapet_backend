e77ba1cbd6ac443fb024f8da6005fdf3b90791de
BL-CoderOvertime <brandonlively@live.com>
2019-06-25 02:55:24 -0400

    Setup Food Entry and Gigapet Repos

---
+++ src/main/java/com/gigapet/backend/repository/FoodEntryRepository.java
@@ -0,0 +1,8 @@
+package com.gigapet.backend.repository;
+
+import com.gigapet.backend.models.Gigapet;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface FoodEntryRepository  extends PagingAndSortingRepository<Gigapet, Long> {
+}
